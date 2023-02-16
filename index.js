09d4b3b3f09c208b20818c8f29d3b4dd0871ad0e
kanybekova eleonora <eelya250@gmail.com>
2023-02-16 14:23:16 +0600

    task output elements from the array

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 let example1 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-let example2 = [[1, 2,3], [4, 5, 6], [7, 8, 9]];
+let example2 = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
+
+console.log(example1.length);
+console.log(example2.length);
+console.log(example1[2]);
+console.log(example2[2]);
