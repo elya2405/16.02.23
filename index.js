0c816a39d90b24a1f9d7705962e4f266a9ef2d63
kanybekova eleonora <eelya250@gmail.com>
2023-02-16 16:00:11 +0600

    calculated the average amount of the cost of cars

--- index.js
+++ index.js
@@ -64,3 +64,16 @@ example7.sort(function (a, b) {
   return a - b; //если надо отсортировать в убывающем порядке делаем b - a
 });
 console.log(example7);
+
+let carPrice = [4000, 3000, 4000, 8000, 5000, 3000, 7000, 12000, 100000, 1000];
+let totalPrice = 0;
+for (let i = 0; i < carPrice.length; i++) {
+  totalPrice = totalPrice + carPrice[i];
+}
+console.log(totalPrice / carPrice.length);
+
+carPrice.sort(function (a, b) {
+  return a - b;
+});
+let medianPrise = (carPrice[4] + carPrice[5]) / 2;
+console.log(medianPrise);
